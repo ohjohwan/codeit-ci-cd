92f3b100a6af254d568b555a47eda738b52a9641
ohjohwan <whghks1020@naver.com>
2024-11-21 16:44:50 +0900

    bable install

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // 최신 JavaScript 코드 변환
+    "@babel/preset-react", // JSX 변환을 위한 프리셋
+  ],
+};
--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   preset: "ts-jest",
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest", // tsx, ts 파일을 ts-jest로 변환
+    "^.+\\.(tsx|ts)$": "ts-jest", // TypeScript 파일을 ts-jest로 변환
+    "^.+\\.(jsx|js)$": "babel-jest", // JSX, JS 파일을 babel-jest로 변환
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!some-package-to-transform)/", // TypeScript 변환을 할 패키지 지정
+    "/node_modules/(?!some-package-to-transform)/", // 변환을 할 패키지 지정
   ],
 };
