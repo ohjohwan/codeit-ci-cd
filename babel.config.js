c661b5f97dc66ee19d12850c4fe92919922d1b17
ohjohwan <whghks1020@naver.com>
2024-11-21 17:14:56 +0900

    isolation false setting

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
-    "@babel/preset-env", // 최신 JavaScript 코드 변환
-    "@babel/preset-react", // JSX 변환을 위한 프리셋
+    "@babel/preset-env", // 최신 JavaScript 변환
+    "@babel/preset-react", // JSX 변환
+    "@babel/preset-typescript", // TypeScript 지원
   ],
 };
